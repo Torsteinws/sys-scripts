0719da26482fa7e1da44de4b9c23e8a71c25dfa2
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2025-11-17 00:39:46 +0100

    Add some logging

--- kWin/ttiler/src/main.ts
+++ kWin/ttiler/src/main.ts
@@ -4,12 +4,19 @@ import type { Shortcut } from "./types/shortcut.js"
 const shortcuts: Shortcut[] = ([] as Shortcut[]).concat(moveAndTile.shortcuts)
 
 shortcuts.forEach((shortcut) => {
-    const fnWithErrorLogger = () => logErrors(shortcut.fn)
+    const shortcutTitle = `ttiler.${shortcut.title}`
+
+    const fnWithLogger = () => {
+        print("Executing shortcut..." + shortcutTitle)
+        logErrors(shortcut.fn)
+        print("Shortcut executed...." + Date())
+    }
+
     registerShortcut(
-        `ttiler.${shortcut.title}`,
+        shortcutTitle,
         shortcut.text,
         shortcut.keySequence,
-        fnWithErrorLogger
+        fnWithLogger
     )
 })
 
