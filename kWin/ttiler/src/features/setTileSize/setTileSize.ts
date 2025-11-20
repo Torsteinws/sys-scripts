932984e244c916abf2221c1a50f0f99067bb040a
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2025-11-20 06:36:32 +0100

    Make it possible to reset tiling layout

--- kWin/ttiler/src/features/setTileSize/setTileSize.ts
+++ kWin/ttiler/src/features/setTileSize/setTileSize.ts
@@ -1,18 +1,38 @@
 import type { Shortcut } from "../../types/shortcut.js"
+import { utils } from "../../utils/index.js"
 
 function debug() {}
 
 function adjustTileWidth(direction: "left" | "right") {
-    const screen = workspace.activeScreen
-    const tileManager = workspace.tilingForScreen(screen)
-    const leftTile = tileManager.bestTileForPosition(screen.geometry.left, screen.geometry.top)
-
+    const leftTile = getTile("left")
     const delta = 0.02
     if (direction === "left") {
         leftTile.relativeGeometry.width -= delta
     } else {
         leftTile.relativeGeometry.width += delta
     }
+}
+
+function resetTileWidth() {
+    const leftTile = getTile("left")
+    leftTile.relativeGeometry.width = 0.62
+}
+
+function resetTileWidthAllDesktops() {
+    const originalDesktop = workspace.currentDesktop
+    workspace.desktops.forEach((desktop) => {
+        workspace.currentDesktop = desktop
+        resetTileWidth()
+    })
+    workspace.currentDesktop = originalDesktop
+    utils.showText("Reset all tiling layouts", "dialog-positive")
+}
+
+function getTile(location: "left" | "right") {
+    const screen = workspace.activeScreen
+    const tileManager = workspace.tilingForScreen(screen)
+    const geometricX = location === "left" ? screen.geometry.left : screen.geometry.right
+    return tileManager.bestTileForPosition(geometricX, screen.geometry.top)
 }
 
 const shortcuts: Shortcut[] = [
@@ -33,6 +53,18 @@ const shortcuts: Shortcut[] = [
         text: "Adjust the tile width in rightward direction",
         keySequence: "Ctrl+Shift+l",
         fn: () => adjustTileWidth("right"),
+    },
+    {
+        title: "setTileSize.resetTileWidth",
+        text: "Reset the tile width on the current virtual desktop",
+        keySequence: "Meta+Ctrl+Alt+'",
+        fn: resetTileWidth,
+    },
+    {
+        title: "setTileSize.resetTileWidthAllDesktops",
+        text: "Reset the tile width on all virtual desktops",
+        keySequence: "Meta+Ctrl+Alt+*",
+        fn: resetTileWidthAllDesktops,
     },
 ]
 
